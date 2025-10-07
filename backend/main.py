39a76187c39453b6f59e90ea7f0af3f416e5f938
hadihassanbaloch <12hadihassan@gmail.com>
2025-10-07 13:57:43 +0500

    Created Frontend Structure

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/healthz")
+async def get_status():
+    return {"Status": "Everything is OK!"}
+
+@app.get("/version")
+async def get_version():
+    return {"CMS Version": "1.0.0"}
+@app.get("/registration")
+async def register_user():
+    return {"message": "User registered successfully"}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+export default function App (){
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Clinic Management System!
+      </h1>
+      <div className="text-2xl font-bold text-center mt-10 text-color-red-500">
+        <li>
+        Step 1 Backend 
+      </li>
+      <li>
+        Step 2 Database integration 
+      </li>
+      <li>
+        Step 3 User Authentication
+      </li>
+
+      </div>
+      
+    </>
+  )
+}
